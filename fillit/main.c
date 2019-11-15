bb398a5867faf2accf9b5aad7491259703372f4b
Salah Adawi <hahli144@hotmail.com>
2019-11-15 22:38:03 +0200

    file reading function

--- fillit/main.c
+++ fillit/main.c
@@ -4,15 +4,24 @@
 #include "fillit.h"
 #include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 static int	store_blocks(t_block **first, char *file)
 {
   int fd;
-  char **buf
+  char **buf;
+  int i;
+
   if ((fd = open(file, O_RDONLY)) == -1)
     return (0);
-  
-  
+  if (!(buf = (char**)malloc(sizeof(*buf) * 4)))
+    return (0);
+  i = 0;
+  while (i < 4)
+    if ((read(fd, buf[i++], 4)) != 4)
+      return (0);
+  if (!check_block(buf)) //function to check if tetromino is valid
+    return (0);
 }
 
 static void	add_block(t_block **first, char **block)
@@ -42,7 +51,7 @@ int		main(int argc, char **argv)
     ft_putstr("./fillit [input_file]\n");
   else
     {
-      if (!store_blocks(blocks, argv[1]))
+      if (!store_blocks(&blocks, argv[1]))
 	ft_putstr("error");
     }
   return (0);
