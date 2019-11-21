0f311d53c041f8aa67668b37a58f4cdc0024827c
Salah Adawi <hahli144@hotmail.com>
2019-11-21 16:34:09 +0200

    fix ft_sqrt to round up

--- fillit/libft/ft_sqrt.c
+++ fillit/libft/ft_sqrt.c
@@ -10,28 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_sqrt(int x)
+int	ft_sqrt(int n)
 {
-	int start;
-	int mid;
-	int end;
-	int ans;
+  int i;
 
-	start = 1;
-	end = x;
-    if (x == 0 || x == 1)  
-       return (x);    
-    while (start <= end)  
-    {         
-        mid = (start + end) / 2; 
-        if (mid * mid == x) 
-            return (mid); 
-        if (mid * mid < x)  
-        { 
-            start = mid + 1; 
-            ans = mid; 
-        }  
-            end = mid - 1;         
-    } 
-    return (ans); 
+  while (i * i < n)
+    i++;
+  return (i);
 }
